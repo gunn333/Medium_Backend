b4e4ac05904fafac5207fbc9811caac49a36e9ff
Gunn Arora <122155330+gunn333@users.noreply.github.com>
2024-09-09 09:47:36 +0530

    Medium

---
+++ login.js
@@ -0,0 +1,8 @@
+function toggleForm() {
+    const loginForm = document.getElementById('login-form');
+    const signupForm = document.getElementById('signup-form');
+    
+    // Toggle visibility between login and signup forms
+    loginForm.classList.toggle('hidden');
+    signupForm.classList.toggle('hidden');
+}s
